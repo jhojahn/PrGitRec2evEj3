36136d707c7ca068f45de029f0feef765876d7bd
unknown <jhojahnb@gmail.com>
2024-04-19 14:31:02 +0200

    version2

--- cliente.java
+++ cliente.java
@@ -15,8 +15,17 @@ private String nombre;
 private int edad;
 private vehiculo[] vehiculos=new vehiculo[2];
 
+cliente(String nombre) {
+	this.nombre=nombre;
+}
+
+cliente(){
+	
+}
+
 void anadir(vehiculo v) {
 	vehiculos[0]=v;
 }
+public static void main(String[] args) {}
 
 }
